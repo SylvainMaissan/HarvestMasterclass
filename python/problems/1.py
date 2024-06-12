03251387a32b974649ed9608c8198383c5fbb010
RichardMiddelkoop <70901129+RichardMiddelkoop@users.noreply.github.com>
2024-06-12 14:53:40 +0200

    Create 1.py

---
+++ python/problems/1.py
@@ -0,0 +1,17 @@
+def calculate_area_of_circle(radius):
+    pi = 3.14159265359
+    area = pi * (radius ** 2)
+    return area
+
+def calculate_area_of_rectangle(length, width):
+    area = length * width
+    return area
+
+def calculate_circumference_of_circle(radius):
+    pi = 3.14159265359
+    circumference = 2 * pi * radius
+    return circumference
+
+def calculate_perimeter_of_rectangle(length, width):
+    perimeter = 2 * (length + width)
+    return perimeter
