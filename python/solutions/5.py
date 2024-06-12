17d2342292ab4342b501819c40f94db3887e0fd0
RichardMiddelkoop <70901129+RichardMiddelkoop@users.noreply.github.com>
2024-06-12 15:08:55 +0200

    Update 5.py

--- python/solutions/5.py
+++ python/solutions/5.py
@@ -2,3 +2,8 @@ def newsletter_sign_up(name, email):
   if name == "":
     print("The name can't be empty!")
     return
+  if email == "":
+    print("The e-mail can't be empty!")
+    return
+  u = User(name, email)
+  newsletter.add(u)
